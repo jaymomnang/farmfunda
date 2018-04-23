1efcfe7d624328451ae3eda65584fd3c1a5c80e8
jaymomnang <juliusmomnang@gmail.com>
2018-04-23 06:59:32 +0100

    name change

--- app.js
+++ app.js
@@ -10,18 +10,19 @@ var express = require('express'),
 global.bodyParser = require('body-parser');
 global.request = require('request');
 global.mc_api = "http://localhost:7000/";
+//global.mc_api = "http://ff-server.appspot.com/";
 global.urlpath = "http://localhost:7100/";
 global.helpers = require('./helpers/helpers');
 
 //configure and create custom nunjucks filters
 engines.requires.nunjucks = nunjucks.configure();
 
-engines.requires.nunjucks.addFilter('shortDate', function(dateValue) {    
+engines.requires.nunjucks.addFilter('shortDate', function(dateValue) {
     var d = dateValue.substring(0, 10);
     return d;
 });
 
-engines.requires.nunjucks.addFilter('addDay', function(value) {    
+engines.requires.nunjucks.addFilter('addDay', function(value) {
     var d = new Date(value);
     d = d.setDate(d.getDate() + 1);
     return d;
@@ -42,6 +43,7 @@ app.set('views', __dirname + '/views');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/resources'));
 app.use(bodyParser.json());
+
 
 app.use(session({ secret: 'as465farmasdwqwdfundazcafd56a5df45a46df22535' }));
 //app.use(errorHandler);
